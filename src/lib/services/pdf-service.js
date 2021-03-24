15e7c919e1aa3245e08352d25cff181b05adaec8
kizaonline <kizaonline@gmail.com>
2021-03-24 13:58:23 +1100

    ON-10882 # Update to use SDK fn for generating pdf

--- src/lib/services/pdf-service.js
+++ src/lib/services/pdf-service.js
@@ -1,37 +1,31 @@
 // @flow
 'use strict'
 
-const axios = require('axios')
+const OneBlink = require('@oneblink/sdk')
 
 const config = require('../../config.js')
-const generateJWT = require('../generate-jwt.js')
 const generateHtml = require('../templates/generate-html.js')
 
 async function generatePDF(submissionData /* : any */) {
-  const URL /* : string */ = config.PDF_SERVICE_URL
   const ACCESS_KEY /* : string */ = config.PDF_ACCESS_KEY
   const SECRET_KEY /* : string */ = config.PDF_SECRET_KEY
-
-  const authorisationToken = await generateJWT(ACCESS_KEY, SECRET_KEY, 300)
 
   const params = Object.assign({}, submissionData)
 
   const html = await generateHtml(params, 'pdf.mustache')
 
-  const instance = axios.create({
-    baseURL: URL,
-    headers: {
-      Authorization: `Bearer ${authorisationToken}`
-    },
-    responseType: 'arraybuffer',
-    maxContentLength: 82428890
-  })
-
   try {
-    const response = await instance.post('/pdf-document', {
-      body: { html: html }
+    const options = {
+      accessKey: ACCESS_KEY,
+      secretKey: SECRET_KEY
+    }
+    const pdf = new OneBlink.PDF(options)
+    const buffer = await pdf.generatePDF({
+      body: {
+        html
+      }
     })
-    return response.data
+    return buffer
   } catch (error) {
     throw new Error(
       `An error has occurred while attempting to generate the PDF: ${error}`
